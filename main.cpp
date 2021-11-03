d8770e28a63a36b245d8d03c30cd28818ca6ca0d
evfloriz <ev.floriz@gmail.com>
2021-11-03 00:29:27 -0600

    Switch to running RunBot in main

--- main.cpp
+++ main.cpp
@@ -6,27 +6,13 @@
 
 #include "BasicSc2Bot.h"
 #include "LadderInterface.h"
-using namespace sc2;
 
+using namespace sc2;
 
 // LadderInterface allows the bot to be tested against the built-in AI or
 // played against other bots
 int main(int argc, char* argv[]) {
-	//RunBot(argc, argv, new BasicSc2Bot(), sc2::Race::Terran);
-	Coordinator coordinator;
-	coordinator.LoadSettings(argc, argv);
-
-	BasicSc2Bot bot;
-	coordinator.SetParticipants({
-		CreateParticipant(Race::Protoss, &bot),
-		CreateComputer(Race::Zerg)
-		});
-
-	coordinator.LaunchStarcraft();
-	coordinator.StartGame();
-
-	while (coordinator.Update()) {
-	}
+	RunBot(argc, argv, new BasicSc2Bot(), sc2::Race::Protoss);
 
 	return 0;
 }
