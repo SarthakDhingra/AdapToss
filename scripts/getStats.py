31532f47ea4a493b2bcc8a96305b29780235fd04
SarthakDhingra <sdhingra@ualberta.ca>
2021-12-06 19:23:20 -0700

    updates

--- scripts/getStats.py
+++ scripts/getStats.py
@@ -78,7 +78,15 @@ def parseOutput(stdout):
 def getGraphs(glob, race, map, difficulty):
 
     # should print to file
+    print('global results')
     print(glob)
+    print('race results')
+    print(race)
+    print('map results')
+    print(map)
+    print('difficulty results')
+    print(difficulty)
+
 
     for key in map:
         map[key.replace('.SC2Map', '')] = map.pop(key)
@@ -125,34 +133,34 @@ def getGraphs(glob, race, map, difficulty):
 
     
 if __name__ == "__main__":
-    runSimulations()
+    #runSimulations()
     #testRun()
 
-    # glob = {'Win': 47, 'Loss': 43, 'Tie': 0, 'Undecided': 0}
+    glob = {'Win': 47, 'Loss': 43, 'Tie': 0, 'Undecided': 0}
 
-    # race = {
-    #     'zerg': {'Win': 14, 'Loss': 16, 'Tie': 0, 'Undecided': 0}, 
-    #     'protoss': {'Win': 17, 'Loss': 13, 'Tie': 0, 'Undecided': 0}, 
-    #     'terran': {'Win': 16, 'Loss': 14, 'Tie': 0, 'Undecided': 0}
-    # }
+    race = {
+        'zerg': {'Win': 14, 'Loss': 16, 'Tie': 0, 'Undecided': 0}, 
+        'protoss': {'Win': 17, 'Loss': 13, 'Tie': 0, 'Undecided': 0}, 
+        'terran': {'Win': 16, 'Loss': 14, 'Tie': 0, 'Undecided': 0}
+    }
 
-    # map = {
-    #     'CactusValleyLE.SC2Map': {'Win': 16, 'Loss': 14, 'Tie': 0, 'Undecided': 0}, 
-    #     'BelShirVestigeLE.SC2Map': {'Win': 17, 'Loss': 13, 'Tie': 0, 'Undecided': 0}, 
-    #     'ProximaStationLE.SC2Map': {'Win': 14, 'Loss': 16, 'Tie': 0, 'Undecided': 0}
-    # }
+    map = {
+        'CactusValleyLE.SC2Map': {'Win': 16, 'Loss': 14, 'Tie': 0, 'Undecided': 0}, 
+        'BelShirVestigeLE.SC2Map': {'Win': 17, 'Loss': 13, 'Tie': 0, 'Undecided': 0}, 
+        'ProximaStationLE.SC2Map': {'Win': 14, 'Loss': 16, 'Tie': 0, 'Undecided': 0}
+    }
 
-    # difficulty = {
-    #     'VeryEasy': {'Win': 9, 'Loss': 0, 'Tie': 0, 'Undecided': 0}, 
-    #     'Easy': {'Win': 9, 'Loss': 0, 'Tie': 0, 'Undecided': 0}, 
-    #     'Medium': {'Win': 8, 'Loss': 1, 'Tie': 0, 'Undecided': 0}, 
-    #     'MediumHard': {'Win': 8, 'Loss': 1, 'Tie': 0, 'Undecided': 0}, 
-    #     'Hard': {'Win': 5, 'Loss': 4, 'Tie': 0, 'Undecided': 0}, 
-    #     'HardVeryHard': {'Win': 5, 'Loss': 4, 'Tie': 0, 'Undecided': 0}, 
-    #     'VeryHard': {'Win': 3, 'Loss': 6, 'Tie': 0, 'Undecided': 0}
-    # }
+    difficulty = {
+        'VeryEasy': {'Win': 9, 'Loss': 0, 'Tie': 0, 'Undecided': 0}, 
+        'Easy': {'Win': 9, 'Loss': 0, 'Tie': 0, 'Undecided': 0}, 
+        'Medium': {'Win': 8, 'Loss': 1, 'Tie': 0, 'Undecided': 0}, 
+        'MediumHard': {'Win': 8, 'Loss': 1, 'Tie': 0, 'Undecided': 0}, 
+        'Hard': {'Win': 5, 'Loss': 4, 'Tie': 0, 'Undecided': 0}, 
+        'HardVeryHard': {'Win': 5, 'Loss': 4, 'Tie': 0, 'Undecided': 0}, 
+        'VeryHard': {'Win': 3, 'Loss': 6, 'Tie': 0, 'Undecided': 0}
+    }
 
-    # getGraphs(glob, race, map, difficulty)
+    getGraphs(glob, race, map, difficulty)
 
 
 # TODO
